8319ee68e5c709c6efdd40109875434bd29aeec5
Sungguk Lim <sungguk.lim@sap.com>
2018-02-05 12:13:56 +0900

    Add root and new path

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var port = process.env.PORT || 1337;
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
  
-/*
 app.get('/', function (req, res) {
-  //res.redirect('https://hdbgerrit.wdf.sap.corp:8443/#/dashboard/self');
-  //res.redirect('https://hdbits.wdf.sap.corp/bugzilla');
-  res.redirect('http://www.limsungguk.com');
+  res.redirect('https://hdbits.wdf.sap.corp/bugzilla/');
 });
-*/
+
+app.get('/new', function(req, res) {
+  res.redirect('https://hdbits.wdf.sap.corp/bugzilla/enter_bug.cgi?product=NewDB');
+});
 
 app.use(function(req, res, next) {
   res.redirect('https://hdbits.wdf.sap.corp/bugzilla/show_bug.cgi?id=' + req.path.slice(1));
